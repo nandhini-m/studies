3b4a9849a2de5c3aa1f3f8eaf6c0aea1d157cbf9
pavithra <nandhinimunusamy149@gmail.com>
2016-07-19 10:19:04 +0530

    Create mul.java

---
+++ mul.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class mul {
+	public static void main(String[] args) {
+		Scanner in1=new Scanner(System.in);
+		int a=in1.nextInt();
+		Scanner in2=new Scanner(System.in);
+		int b=in1.nextInt();
+		int rem=0,diff=0;
+		while(a>=b)
+		{
+			diff=a-b;
+			a=diff;
+			rem++;
+		}
+		System.out.println("quotient :"+rem+" rem:"+diff);
+		}
+	}
+
+	  
