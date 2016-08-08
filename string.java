82bb83874b37627415f1af620f91ddc85e58a1eb
pavithra <nandhinimunusamy149@gmail.com>
2016-08-08 03:38:19 -0700

    Create string.java

---
+++ string.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class string {
+	public static void main(String[] args) {
+		
+Scanner in=new Scanner(System.in);
+System.out.println("enter a string:");
+String s=in.next();
+StringBuffer sb=new StringBuffer(s);
+sb.reverse();
+System.out.println("reversed string is "+sb );
+	}
+}
+
